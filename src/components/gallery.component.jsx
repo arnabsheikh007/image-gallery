7d5b77a01ca957255dc31e7c6bde141f3617f9a5
Sheikh Bahauddin Arnab <arnab.sheikh007@gmail.com>
2023-11-02 00:03:36 +0600

    Added Delete item feature
    Signed-off-by: Sheikh Bahauddin Arnab <arnab.sheikh007@gmail.com>

--- src/components/gallery.component.jsx
+++ src/components/gallery.component.jsx
@@ -1,32 +1,63 @@
+import React, { useState } from "react";
+
 import ImageContainer from "./imageContainer.component";
 import "./gallery.component.css";
 
-
-
-const Gallery =() => {
-  
-  const deleteItem = () => {
-    
-  }
+const Gallery = () => {
   const images = require.context("../Assets", true);
-  const imageList = images.keys().map(image => images(image));
-  
+  const data = images.keys().map((image) => images(image));
 
+  const [imageList, setImageList] = useState(data);
+  const [selected, setSelected] = useState(0);
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const txt = `${selected} item${selected > 1 ? "s" : ""} selected`;
+  const showDelete = selected > 0;
+
+  const handleCheckboxChange = (image) => {
+    if (selectedImages.includes(image)) {
+      setSelectedImages(selectedImages.filter((img) => img !== image));
+      setSelected(selected - 1);
+    } else {
+      setSelectedImages([...selectedImages, image]);
+      setSelected(selected + 1);
+    }
+  };
+
+  const deleteItem = () => {
+    const updatedImage = imageList.filter(
+      (img) => !selectedImages.includes(img)
+    );
+    setImageList(updatedImage);
+    setSelectedImages([]);
+    setSelected(0);
+  };
+  // console.log(imageList);
   return (
     <div className="Gallery">
+      {showDelete ? (
         <div className="heading">
-            <h1>Gallery</h1>
-            <p onClick={deleteItem}>Delete items</p>
+          <h2>{txt}</h2>
+          <p onClick={deleteItem}>Delete items</p>
         </div>
-        <hr />
-        <div className="image-container">
-          {imageList.map((image, index) => (
-            <ImageContainer key={index} image={image}/>
-          ))}
+      ) : (
+        <div className="heading">
+          <h2>Gallery</h2>
         </div>
-        
+      )}
+      <hr />
+      <div className="image-container">
+        {imageList.map((image) => (
+          <ImageContainer
+            key={image}
+            image={image}
+            onCheckboxChange={handleCheckboxChange}
+            isChecked={selectedImages.includes(image)}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Gallery;
--- src/components/imageContainer.component.jsx
+++ src/components/imageContainer.component.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import './imageContainer.component.css';
+import React from "react";
+import "./imageContainer.component.css";
 
-const ImageContainer =(props) => {
+const ImageContainer = (props) => {
+  const { image, onCheckboxChange, isChecked } = props;
+
+  const handleCheckboxChange = () => {
+    onCheckboxChange(image);
+  };
   return (
-    <div className="container"> 
-      <input type="checkbox" className='overlay-checkbox'/>
+    <div className="container">
+      <input
+        type="checkbox"
+        className="overlay-checkbox"
+        onChange={handleCheckboxChange}
+        checked={isChecked}
+      />
       <label htmlFor="checkbox" className="overlay-div"></label>
-      <img src={props.image} alt="hello" />
+      <img src={props.image} alt="img" />
     </div>
   );
-}
+};
 
 export default ImageContainer;
